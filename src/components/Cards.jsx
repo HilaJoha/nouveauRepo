afcafb57f5354cea7c2a169b4cf2f2455db9b6b0
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-07 10:03:03 +0200

    solving shit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+function Cards () {
+
+    return (
+        <div className="card-container">
+            <div className="card">
+                <div className="border">
+                    <h1>Palmistry</h1>
+                    <img src="https://i.pinimg.com/736x/4f/b2/70/4fb270df2c1f8ea42f70a938f7d6d8c6.jpg" className="hand"></img>
+                
+                  
+                </div>
+
+            </div>
+        </div>
+    
+    ) };
+
+export default Cards;
